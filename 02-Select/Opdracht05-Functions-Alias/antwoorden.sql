578b0285fd1092b7ab695237364e36d282ff451d
2154558 <90393278+2154558@users.noreply.github.com>
2022-01-27 10:34:27 +0100

    opgave 5 verbeterd
    aliasen toegevoegd + foutieve opgaven verbeterd

--- 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
+++ 02-Select/Opdracht05-Functions-Alias/antwoorden.sql
@@ -1,29 +1,29 @@
 -- Opdracht 1 
-SELECT max(wage) FROM  players
+SELECT max(wage) AS hoogste_loon_speler_fc_utrecht.  FROM  club ="FC Utrecht"
 
 -- Opdracht 2 
-SELECT max(wage) FROM  players WHERE club ="FC Utrecht"
+SELECT AVG(wage) AS gem_loon FROM  players WHERE club ="FC Utrecht"
 
 -- Opdracht 3
 SELECT avg(wage) FROM  players 
 
 -- Opdracht 4
-SELECT sum(wage) FROM  players WHERE club = "FC Groningen"
+SELECT sum(wage) AS totaal_loon FROM  players WHERE club = "FC Groningen"
 
 -- Opdracht 5
-SELECT COUNT(name) FROM  players WHERE club = "Manchester City" OR club = "Manchester United";
+SELECT COUNT(name) AS aantal_spelers FROM  players WHERE club = "Manchester City" OR club = "Manchester United";
 
 -- Opdracht 6 
-SELECT AVG(wage) FROM  players WHERE age <"20" 
+SELECT AVG(wage) AS gem_wage_jonger_dan_20 FROM  players WHERE age <"20" 
 
 -- Opdracht 7 
-SELECT AVG(wage) FROM  players WHERE age >"20" 
+SELECT AVG(wage) AS gem_wage_ouder_dan_20 FROM  players WHERE age >"20" 
 
 -- Opdracht 8
 SELECT SUM(wage) FROM  players WHERE club ="Chelsea"
 
 -- Opdracht 9
-SELECT round(AVG(age)) FROM  players 
+SELECT round(AVG(age)) AS gem_leeftijd FROM  players 
 
 -- Opdracht 10
-SELECT club, SUM(wage), ROUND(SUM(value)) FROM  players WHERE club ="Liverpool"
+SELECT club AS clubnaam , SUM(wage) AS totaal_loon, ROUND(SUM(value)) AS waarde FROM  players WHERE club ="Liverpool"
